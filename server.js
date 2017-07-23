2952ae50c782a3abb5092726c1cddd4e187a1381
Ritish Gumber <ritishgumber@gmail.com>
2017-07-23 14:16:10 +0530

    resume pdf added

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 var express = require("express");
-module.exports = function() {
+var path = require('path')
+module.exports = function () {
     var app = express();
     app.set('port', process.env.PORT || 8888);
 
     app.use(express.static('public'));
-
+    app.use(express.static('public'));
     app.engine('html', require('ejs').renderFile);
 
-    app.get('/', function(req, res) {
-        res.redirect("https://ritishdocs.s3.amazonaws.com/Ritish%20Gumber%20resume.pdf1497362808787");
+    app.get('/', function (req, res) {
+        // res.sendFile("https://ritishdocs.s3.amazonaws.com/Ritish%20Gumber%20resume.pdf1497362808787");
+        res.sendFile((path.join(__dirname, 'public', 'resume.pdf')));
     });
     return app;
 
