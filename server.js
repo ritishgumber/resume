799d4aedad062ea78daaf3d5fae88afd1b3aa56e
Ritish Gumber <ritishgumber@gmail.com>
2017-07-23 17:38:12 +0530

    status api added

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ module.exports = function () {
     app.use(express.static('public'));
     app.use(express.static('public'));
     app.engine('html', require('ejs').renderFile);
-
+    app.get('/status', (req, res) => {
+        res.status(200).send({
+            status: 200,
+            message: 'Ok'
+        })
+    })
     app.get('/', function (req, res) {
         // res.sendFile("https://ritishdocs.s3.amazonaws.com/Ritish%20Gumber%20resume.pdf1497362808787");
         res.sendFile((path.join(__dirname, 'public', 'resume.pdf')));
