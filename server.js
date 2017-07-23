b79f836dc4b28fdf26bcc191cc8bd7c978cc3c12
Ritish Gumber <ritishgumber@gmail.com>
2017-07-23 21:32:25 +0530

    cron removed

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 var express = require("express");
 var path = require('path')
-var cronjob = require('cron-job');
-const axios = require('axios')
 module.exports = function () {
     var app = express();
     app.set('port', process.env.PORT || 8888);
-    cronjob.startJobEveryTimegap(cronjob.date_util.getNowTimestamp(), 28 * 60, () => {
-        axios.get('http://resume.ritishgumber.me').then((response) => {
-            console.log('ritish resume', response.status);
-        }, (err) => {
-            console.log('resume err')
-        })
-    });
     app.use(express.static('public'));
     app.use(express.static('public'));
     app.engine('html', require('ejs').renderFile);
@@ -22,7 +13,6 @@ module.exports = function () {
         })
     })
     app.get('/', function (req, res) {
-        // res.sendFile("https://ritishdocs.s3.amazonaws.com/Ritish%20Gumber%20resume.pdf1497362808787");
         res.sendFile((path.join(__dirname, 'public', 'resume.pdf')));
     });
     return app;
